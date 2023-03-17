704b169467306a54a4722dec88f1b3af2282dc1e
YazanAlhalabi <yazanalhalabi1@gmail.com>
2023-03-17 14:44:34 +0200

    added MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Button, Box, Grid, Typography } from '@mui/material'
 
-import './App.css'
 import { decrement, increment } from './features/counter/counterSlice'
 import { RootState } from './store'
+import './App.css'
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value)
@@ -11,12 +11,24 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Vite + React + Toolkit + Tailwind</h1>
-      <div className="card">
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <span className="px-10">{count}</span>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-      </div>
+      <h1>Vite + React + Toolkit + MUI</h1>
+      <Box sx={{ width: '100%' }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={5}>
+            <Button variant="contained" onClick={() => dispatch(increment())}>
+              Increment
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography>{count}</Typography>
+          </Grid>
+          <Grid item xs={5}>
+            <Button variant="contained" onClick={() => dispatch(decrement())}>
+              Decrement
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
